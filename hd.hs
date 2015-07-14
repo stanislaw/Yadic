4c28dfd959e69744a66187472e7a26f85b04db43
Stanislaw Pankevich <s.pankevich@gmail.com>
2015-07-14 21:50:08 +0300

    Initial function structure to fetch word translation

--- hd.hs
+++ hd.hs
@@ -1,4 +1,23 @@
 module Main where
 
-main = putStrLn "Hello, World!"
+import System.Environment(getArgs)
+
+performHttpRequest :: String -> IO ()
+performHttpRequest request = do
+  putStrLn request
+
+wordHttpRequest :: String -> String
+wordHttpRequest word = word
+
+getTranslation :: String -> IO ()
+getTranslation word = performHttpRequest (wordHttpRequest word)
+
+main = do 
+  args <- getArgs
+  case args of
+    [word] -> do
+      getTranslation word
+    
+    _ -> do
+      putStrLn "Enter a word"
 
